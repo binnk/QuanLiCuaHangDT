94e71f0ce5048d3aa43ce283157333aaf1c15620
binnk <phamtankhang2001@gmail.com>
2020-11-06 16:48:05 +0700

    Edit form

--- QLCH_UI/GUI/AddCustomerForm.Designer.cs
+++ QLCH_UI/GUI/AddCustomerForm.Designer.cs
@@ -31,6 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lbNgSinh = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tbxDiaChi = new System.Windows.Forms.TextBox();
+            this.lbDiaChi = new System.Windows.Forms.Label();
             this.cbxGtinh = new System.Windows.Forms.ComboBox();
             this.lbGtinh = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -39,12 +43,10 @@
             this.tbxTenKH = new System.Windows.Forms.TextBox();
             this.lbTenKH = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.tbxDiaChi = new System.Windows.Forms.TextBox();
-            this.lbDiaChi = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.lbNgSinh = new System.Windows.Forms.Label();
             this.btnLuu = new System.Windows.Forms.Button();
             this.btnHuy = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,6 +61,8 @@
             // 
             this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.lbNgSinh);
             this.panel1.Controls.Add(this.dateTimePicker1);
             this.panel1.Controls.Add(this.tbxDiaChi);
@@ -72,30 +76,72 @@
             this.panel1.Controls.Add(this.lbTenKH);
             this.panel1.Location = new System.Drawing.Point(12, 78);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(385, 395);
+            this.panel1.Size = new System.Drawing.Size(533, 449);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // lbNgSinh
+            // 
+            this.lbNgSinh.AutoSize = true;
+            this.lbNgSinh.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNgSinh.ForeColor = System.Drawing.Color.Black;
+            this.lbNgSinh.Location = new System.Drawing.Point(22, 135);
+            this.lbNgSinh.Name = "lbNgSinh";
+            this.lbNgSinh.Size = new System.Drawing.Size(140, 32);
+            this.lbNgSinh.TabIndex = 14;
+            this.lbNgSinh.Text = "Ngày sinh";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 18F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(28, 170);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(217, 49);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // tbxDiaChi
+            // 
+            this.tbxDiaChi.Font = new System.Drawing.Font("Arial", 18F);
+            this.tbxDiaChi.Location = new System.Drawing.Point(24, 362);
+            this.tbxDiaChi.Name = "tbxDiaChi";
+            this.tbxDiaChi.Size = new System.Drawing.Size(478, 49);
+            this.tbxDiaChi.TabIndex = 12;
+            // 
+            // lbDiaChi
+            // 
+            this.lbDiaChi.AutoSize = true;
+            this.lbDiaChi.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDiaChi.ForeColor = System.Drawing.Color.Black;
+            this.lbDiaChi.Location = new System.Drawing.Point(22, 327);
+            this.lbDiaChi.Name = "lbDiaChi";
+            this.lbDiaChi.Size = new System.Drawing.Size(100, 32);
+            this.lbDiaChi.TabIndex = 11;
+            this.lbDiaChi.Text = "Địa chỉ";
+            // 
             // cbxGtinh
             // 
-            this.cbxGtinh.Font = new System.Drawing.Font("Arial", 14F);
+            this.cbxGtinh.Font = new System.Drawing.Font("Arial", 18F);
             this.cbxGtinh.FormattingEnabled = true;
             this.cbxGtinh.Items.AddRange(new object[] {
             "Nam",
             "Nữ",
             "Khác"});
-            this.cbxGtinh.Location = new System.Drawing.Point(247, 147);
+            this.cbxGtinh.Location = new System.Drawing.Point(274, 170);
             this.cbxGtinh.Name = "cbxGtinh";
-            this.cbxGtinh.Size = new System.Drawing.Size(99, 40);
+            this.cbxGtinh.Size = new System.Drawing.Size(228, 49);
             this.cbxGtinh.TabIndex = 10;
             // 
             // lbGtinh
             // 
             this.lbGtinh.AutoSize = true;
-            this.lbGtinh.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbGtinh.Location = new System.Drawing.Point(243, 123);
+            this.lbGtinh.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGtinh.ForeColor = System.Drawing.Color.Black;
+            this.lbGtinh.Location = new System.Drawing.Point(270, 135);
             this.lbGtinh.Name = "lbGtinh";
-            this.lbGtinh.Size = new System.Drawing.Size(85, 23);
+            this.lbGtinh.Size = new System.Drawing.Size(123, 32);
             this.lbGtinh.TabIndex = 9;
             this.lbGtinh.Text = "Giới tính";
             // 
@@ -110,89 +156,51 @@
             // 
             // tbxSDT
             // 
-            this.tbxSDT.Font = new System.Drawing.Font("Arial", 14F);
-            this.tbxSDT.Location = new System.Drawing.Point(24, 233);
+            this.tbxSDT.Font = new System.Drawing.Font("Arial", 18F);
+            this.tbxSDT.Location = new System.Drawing.Point(28, 269);
             this.tbxSDT.Name = "tbxSDT";
-            this.tbxSDT.Size = new System.Drawing.Size(191, 40);
+            this.tbxSDT.Size = new System.Drawing.Size(217, 49);
             this.tbxSDT.TabIndex = 7;
             // 
             // lbSDT
             // 
             this.lbSDT.AutoSize = true;
-            this.lbSDT.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbSDT.Location = new System.Drawing.Point(20, 207);
+            this.lbSDT.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSDT.Location = new System.Drawing.Point(22, 234);
             this.lbSDT.Name = "lbSDT";
-            this.lbSDT.Size = new System.Drawing.Size(124, 23);
+            this.lbSDT.Size = new System.Drawing.Size(178, 32);
             this.lbSDT.TabIndex = 6;
             this.lbSDT.Text = "Số điện thoại";
             // 
             // tbxTenKH
             // 
-            this.tbxTenKH.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxTenKH.Font = new System.Drawing.Font("Arial", 18F);
             this.tbxTenKH.ForeColor = System.Drawing.Color.Black;
             this.tbxTenKH.Location = new System.Drawing.Point(24, 70);
             this.tbxTenKH.Name = "tbxTenKH";
-            this.tbxTenKH.Size = new System.Drawing.Size(322, 40);
+            this.tbxTenKH.Size = new System.Drawing.Size(478, 49);
             this.tbxTenKH.TabIndex = 5;
             // 
             // lbTenKH
             // 
             this.lbTenKH.AutoSize = true;
-            this.lbTenKH.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTenKH.Location = new System.Drawing.Point(20, 44);
+            this.lbTenKH.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenKH.ForeColor = System.Drawing.Color.Black;
+            this.lbTenKH.Location = new System.Drawing.Point(18, 35);
             this.lbTenKH.Name = "lbTenKH";
-            this.lbTenKH.Size = new System.Drawing.Size(148, 23);
+            this.lbTenKH.Size = new System.Drawing.Size(219, 32);
             this.lbTenKH.TabIndex = 4;
             this.lbTenKH.Text = "Tên khách hàng";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(71, 26);
+            this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(107, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 37);
+            this.label1.Size = new System.Drawing.Size(351, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Thêm khách hàng";
-            // 
-            // tbxDiaChi
-            // 
-            this.tbxDiaChi.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxDiaChi.Location = new System.Drawing.Point(24, 309);
-            this.tbxDiaChi.Name = "tbxDiaChi";
-            this.tbxDiaChi.Size = new System.Drawing.Size(322, 40);
-            this.tbxDiaChi.TabIndex = 12;
-            // 
-            // lbDiaChi
-            // 
-            this.lbDiaChi.AutoSize = true;
-            this.lbDiaChi.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbDiaChi.Location = new System.Drawing.Point(20, 283);
-            this.lbDiaChi.Name = "lbDiaChi";
-            this.lbDiaChi.Size = new System.Drawing.Size(69, 23);
-            this.lbDiaChi.TabIndex = 11;
-            this.lbDiaChi.Text = "Địa chỉ";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-            this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 14F);
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(24, 148);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.ShowUpDown = true;
-            this.dateTimePicker1.Size = new System.Drawing.Size(217, 40);
-            this.dateTimePicker1.TabIndex = 13;
-            // 
-            // lbNgSinh
-            // 
-            this.lbNgSinh.AutoSize = true;
-            this.lbNgSinh.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNgSinh.Location = new System.Drawing.Point(20, 123);
-            this.lbNgSinh.Name = "lbNgSinh";
-            this.lbNgSinh.Size = new System.Drawing.Size(95, 23);
-            this.lbNgSinh.TabIndex = 14;
-            this.lbNgSinh.Text = "Ngày sinh";
             // 
             // btnLuu
             // 
@@ -200,7 +208,7 @@
             this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLuu.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLuu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLuu.Location = new System.Drawing.Point(273, 498);
+            this.btnLuu.Location = new System.Drawing.Point(421, 547);
             this.btnLuu.Name = "btnLuu";
             this.btnLuu.Size = new System.Drawing.Size(124, 47);
             this.btnLuu.TabIndex = 8;
@@ -213,7 +221,7 @@
             this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHuy.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHuy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnHuy.Location = new System.Drawing.Point(153, 498);
+            this.btnHuy.Location = new System.Drawing.Point(281, 547);
             this.btnHuy.Name = "btnHuy";
             this.btnHuy.Size = new System.Drawing.Size(124, 47);
             this.btnHuy.TabIndex = 9;
@@ -221,12 +229,29 @@
             this.btnHuy.UseVisualStyleBackColor = true;
             this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 18F);
+            this.textBox1.Location = new System.Drawing.Point(274, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 49);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 32);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "CMND";
+            // 
             // AddCustomerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(411, 582);
+            this.ClientSize = new System.Drawing.Size(562, 622);
             this.Controls.Add(this.btnHuy);
             this.Controls.Add(this.btnLuu);
             this.Controls.Add(this.label1);
@@ -261,5 +286,7 @@
         private System.Windows.Forms.Label lbDiaChi;
         private System.Windows.Forms.Button btnHuy;
         private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
