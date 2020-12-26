e74c225460d69e3595975ec74ec1ed9127f254cb
binnk <phamtankhang2001@gmail.com>
2020-12-26 18:46:06 +0700

    fix fChangePassword

---
+++ QLCH_UI/GUI/fChangePassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCH_UI
+{
+    public partial class fChangePassword : Form
+    {
+        public fChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void gunaButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
